3e9a0870299c7639272fb282269b15b877956790
Aron Borquez <aron.borquezk@gmail.com>
2026-05-28 21:15:47 +0000

    KAN-2 Responsable de la logica algoritmica

--- script/analisis_climatico.py
+++ script/analisis_climatico.py
@@ -5,5 +5,7 @@ archivo = open("datos/annual2010-2026.csv")
 
 lector = csv.reader(archivo)
 
+encabezado = next(lector)
+
 for fila in lector:
-    print(fila)
+    print("Año:", fila[1], "- Temperatura promedio:", fila[2])
