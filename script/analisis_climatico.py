e9be55efafb9f23b5cbf1ce965d85a2a70ff4c51
Aron Borquez <aron.borquezk@gmail.com>
2026-05-30 13:47:35 +0000

    KAN-2 Responsable de la logica algoritmica

--- script/analisis_climatico.py
+++ script/analisis_climatico.py
@@ -13,15 +13,9 @@ temperatura_minima = 999
 anio_maximo = ""
 anio_minimo = ""
 
-suma_temperaturas = 0
-cantidad = 0
-
 for fila in lector:
 
     temperatura = float(fila[2])
-
-    suma_temperaturas = suma_temperaturas + temperatura
-    cantidad = cantidad + 1
 
     if temperatura > temperatura_maxima:
         temperatura_maxima = temperatura
@@ -30,8 +24,6 @@ for fila in lector:
     if temperatura < temperatura_minima:
         temperatura_minima = temperatura
         anio_minimo = fila[1]
-
-promedio = suma_temperaturas / cantidad
 
 print("Temperatura mas alta:")
 print(temperatura_maxima)
@@ -44,6 +36,3 @@ print(temperatura_minima)
 
 print("Año de temperatura minima:")
 print(anio_minimo)
-
-print("Promedio general de temperaturas:")
-print(promedio)
